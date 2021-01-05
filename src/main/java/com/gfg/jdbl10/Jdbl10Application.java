aed2784a9336b5593dbe94ab07c2572d9086703b
Suyash Verma <verma.suyash22@gmail.com>
2021-01-05 22:39:38 +0530

    first-commit

---
+++ src/main/java/com/gfg/jdbl10/Jdbl10Application.java
@@ -0,0 +1,12 @@
+package com.gfg.jdbl10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdbl10Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Jdbl10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfg/jdbl10/Jdbl10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfg.jdbl10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdbl10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
